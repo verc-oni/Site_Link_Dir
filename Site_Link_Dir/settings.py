22bcb1796e296cde26e893eab5900259edfa1c7f
verc-oni <89543105+verc-oni@users.noreply.github.com>
2021-10-11 17:54:57 +0100

    Update settings.py

--- Site_Link_Dir/settings.py
+++ Site_Link_Dir/settings.py
@@ -1,4 +1,3 @@
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -113,3 +112,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
